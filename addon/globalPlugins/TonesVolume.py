2ad0f1ab9bdf19439dd39d3bd2b9ede547a27700
Kvark <kvark128@yandex.ru>
2023-04-20 01:13:07 +0800

    Volume is now adjustable via a slider

--- addon/globalPlugins/TonesVolume.py
+++ addon/globalPlugins/TonesVolume.py
@@ -12,34 +12,33 @@ from gui import SettingsPanel, NVDASettingsDialog, guiHelper, nvdaControls
 addonHandler.initTranslation()
 
 class AddonSettingsPanel(SettingsPanel):
-	title = _("Tones volume")
+	title = _("Tones Volume")
 
 	def makeSettings(self, settingsSizer):
 		sHelper = guiHelper.BoxSizerHelper(self, sizer=settingsSizer)
-		self.volumeFactor = sHelper.addLabeledControl(_("Volume factor:"),
-			nvdaControls.SelectOnFocusSpinCtrl, min=-50, max=50,
-			initial=config.conf["TonesVolume"]["volumeFactor"])
+		self.volumeSlider = sHelper.addLabeledControl(_("Volume:"),
+			nvdaControls.EnhancedInputSlider, value=config.conf["TonesVolume"]["volume"])
 
 	def postInit(self):
-		self.volumeFactor.SetFocus()
+		self.volumeSlider.SetFocus()
 
 	def onSave(self):
-		config.conf["TonesVolume"]["volumeFactor"] = self.volumeFactor.GetValue()
+		config.conf["TonesVolume"]["volume"] = self.volumeSlider.GetValue()
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	def __init__(self):
 		super().__init__()
 		config.conf.spec["TonesVolume"] = {
-			"volumeFactor": "integer(default=0,min=-50,max=50)",
+			"volume": "integer(default=50,min=0,max=100)",
 		}
 		NVDASettingsDialog.categoryClasses.append(AddonSettingsPanel)
 		NVDAHelper.generateBeep = self.generateBeepWrapper(NVDAHelper.generateBeep)
 
 	def generateBeepWrapper(self, generateBeep):
 		def customGenerateBeep(buf, hz, length, left, right):
-			left = left * (config.conf["TonesVolume"]["volumeFactor"] + 50) // 50
-			right = right * (config.conf["TonesVolume"]["volumeFactor"] + 50) // 50
+			left = left * config.conf["TonesVolume"]["volume"] // 50
+			right = right * config.conf["TonesVolume"]["volume"] // 50
 			return generateBeep(buf, hz, length, min(left, 100), min(right, 100))
 		return customGenerateBeep
 
